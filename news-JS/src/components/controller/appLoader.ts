d390aa078485ab05611225d0be9fc95bc8570a9e
Shahzod222 <shahzodubaydullaev2206@gmail.com>
2022-07-02 23:08:54 +0500

    fix: try to fix api error

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '3000231c228d4d13b42806036c3a767f',
         });
     }
