e23262122e51d4abbdb58c397d2a96b49a674f5a
Shahzod222 <shahzodubaydullaev2206@gmail.com>
2022-08-02 13:33:16 +0500

    fix: split large functions into small ones

--- async-race/src/getCars&Winners/race.ts
+++ async-race/src/getCars&Winners/race.ts
@@ -1,40 +1,32 @@
-import { pageNumber } from '../others/renderHtml';
+import { pageNumber, resetBtn, startBtn } from '../others/renderHtml';
 import { garage, ICars } from '../types/types';
 import { getButton, start, stopRace } from './startStop';
 
-export function race() {
-    const startBtn = document.querySelector('.start-race') as HTMLButtonElement;
-    const resetBtn = document.querySelector('.reset-race') as HTMLButtonElement;
+startBtn.addEventListener('click', async () => {
+    startBtn.disabled = true;
+    resetBtn.disabled = false;
+
+    const res: Response = await garage.getLimitedCars(Number(pageNumber.innerHTML));
+    const cars = (await res.json()) as ICars[];
+    cars.forEach((el) => {
+        const ABtn = getButton(document.querySelectorAll('.car-start') as NodeListOf<HTMLButtonElement>, String(el.id));
+        const stopBtns = document.querySelectorAll('.car-stop') as NodeListOf<HTMLButtonElement>;
+
+        start(ABtn, String(el.id), stopBtns, startBtn, resetBtn);
+    });
+});
+
+resetBtn.addEventListener('click', async () => {
     resetBtn.disabled = true;
-
-    startBtn.addEventListener('click', async () => {
-        startBtn.disabled = true;
-        resetBtn.disabled = false;
-
-        const res: Response = await garage.getLimitedCars(Number(pageNumber.innerHTML));
-        const cars = (await res.json()) as ICars[];
-        cars.forEach((el) => {
-            const ABtn = getButton(
-                document.querySelectorAll('.car-start') as NodeListOf<HTMLButtonElement>,
-                String(el.id)
-            );
-            const stopBtns = document.querySelectorAll('.car-stop') as NodeListOf<HTMLButtonElement>;
-
-            start(ABtn, String(el.id), stopBtns, startBtn, resetBtn);
-        });
+    startBtn.disabled = false;
+    const res: Response = await garage.getLimitedCars(Number(pageNumber.innerHTML));
+    const cars = (await res.json()) as ICars[];
+    cars.forEach((el) => {
+        const startBtns = document.querySelectorAll('.car-start') as NodeListOf<HTMLButtonElement>;
+        const stopBtn = getButton(
+            document.querySelectorAll('.car-stop') as NodeListOf<HTMLButtonElement>,
+            String(el.id)
+        );
+        stopRace(stopBtn, String(el.id), startBtns);
     });
-    resetBtn.addEventListener('click', async () => {
-        resetBtn.disabled = true;
-        startBtn.disabled = false;
-        const res: Response = await garage.getLimitedCars(Number(pageNumber.innerHTML));
-        const cars = (await res.json()) as ICars[];
-        cars.forEach((el) => {
-            const startBtns = document.querySelectorAll('.car-start') as NodeListOf<HTMLButtonElement>;
-            const stopBtn = getButton(
-                document.querySelectorAll('.car-stop') as NodeListOf<HTMLButtonElement>,
-                String(el.id)
-            );
-            stopRace(stopBtn, String(el.id), startBtns);
-        });
-    });
-}
+});
--- async-race/src/getCars&Winners/startStop.ts
+++ async-race/src/getCars&Winners/startStop.ts
@@ -49,21 +49,53 @@ export async function start(
     startBtn?: HTMLButtonElement,
     resetBtn?: HTMLButtonElement
 ) {
-    const car = document.getElementById(id) as HTMLDivElement;
-    const icon = car.querySelector(`#car-${id}`) as HTMLSpanElement;
-    const parent = document.querySelector('.race-road') as HTMLDivElement;
-    const stopBtn = getButton(stopBtns, id);
-    const lWheel = icon.querySelector('.l-wheel') as HTMLSpanElement;
-    const rWheel = icon.querySelector('.r-wheel') as HTMLSpanElement;
-    const smoke = icon.querySelector('.car-smoke') as HTMLSpanElement;
-
+    const [car, icon, parent, stopBtn, lWheel, rWheel, smoke] = getElements(id, stopBtns);
     stopBtn.disabled = false;
-
     el.disabled = true;
 
     const start = (await enigine.start(Number(id))) as unknown as IEngineStart;
     const time = start.distance / start.velocity;
 
+    const [animation, elPosition, lW, rW] = animations(car, lWheel, rWheel, time, parent);
+
+    const resetClicked = false;
+
+    resetBtn?.addEventListener('click', () => {
+        resetOrStop(lW, rW, animation, elPosition, smoke, resetClicked, stopBtn);
+    });
+
+    stopBtn.addEventListener('click', () => {
+        resetOrStop(lW, rW, animation, elPosition, smoke, resetClicked, stopBtn);
+    });
+
+    const driveStatus = await enigine.drive(Number(id));
+    if (!resetClicked) {
+        if (driveStatus === 500) {
+            status500(lW, rW, animation, car, elPosition, smoke);
+            if (startBtn) startBtn.disabled = true;
+        } else if (driveStatus === 200) {
+            enigine.stop(Number(id));
+            if (startBtn) {
+                stopBtn.disabled = false;
+                el.disabled = true;
+            }
+            if (!winner && startBtn) status200(car, time);
+        }
+    }
+}
+
+export function getButton(b: NodeListOf<HTMLButtonElement>, n: string) {
+    const btn = [...b].find((el) => el.name === n) as HTMLButtonElement;
+    return btn;
+}
+
+function animations(
+    car: HTMLDivElement,
+    lWheel: HTMLSpanElement,
+    rWheel: HTMLSpanElement,
+    time: number,
+    parent: HTMLDivElement
+): [Animation, ReturnType<typeof setTimeout>, Animation, Animation] {
     const animation = car.animate(
         [
             {
@@ -84,67 +116,77 @@ export async function start(
     const elPosition = setTimeout(() => {
         car.style.left = `${parent.clientWidth - 163}px`;
     }, time);
-
-    let resetClicked = false;
-    resetBtn?.addEventListener('click', () => {
-        animation.cancel();
-        clearTimeout(elPosition);
-        lW.cancel();
-        rW.cancel();
-        smoke.style.display = 'none';
-        stopBtn.disabled = true;
-        resetClicked = true;
-    });
-
-    stopBtn.addEventListener('click', () => {
-        animation.cancel();
-        clearTimeout(elPosition);
-        lW.cancel();
-        rW.cancel();
-        smoke.style.display = 'none';
-        stopBtn.disabled = true;
-        resetClicked = true;
-    });
-
-    const driveStatus = await enigine.drive(Number(id));
-    if (!resetClicked) {
-        if (driveStatus === 500) {
-            lW.cancel();
-            rW.cancel();
-            animation.pause();
-            car.style.left = getComputedStyle(car).left;
-            animation.cancel();
-            clearTimeout(elPosition);
-            smoke.style.display = 'block';
-
-            if (startBtn) startBtn.disabled = true;
-        } else if (driveStatus === 200) {
-            enigine.stop(Number(id));
-
-            if (startBtn) {
-                stopBtn.disabled = false;
-                el.disabled = true;
-            }
-
-            if (!winner && startBtn) {
-                winner = Number(car.id);
-                const add = winners.addWinner(winner, Math.round(time / 1000));
-                showModal(winner, Math.round(time / 1000));
-
-                if ((await add).status === 500) {
-                    const get = (await winners.getWinner(winner)) as IWinner;
-                    const winCount = get.wins + 1;
-                    const bestTime = Math.round(time / 1000) > get.time ? get.time : Math.round(time / 1000);
-                    winners.updateWinner(winner, winCount, bestTime);
-                }
-
-                showWinners();
-            }
-        }
-    }
+    return [animation, elPosition, lW, rW];
 }
 
-export function getButton(b: NodeListOf<HTMLButtonElement>, n: string) {
-    const btn = [...b].find((el) => el.name === n) as HTMLButtonElement;
-    return btn;
+function status500(
+    lW: Animation,
+    rW: Animation,
+    animation: Animation,
+    car: HTMLDivElement,
+    elPosition: ReturnType<typeof setTimeout>,
+    smoke: HTMLSpanElement
+) {
+    lW.cancel();
+    rW.cancel();
+    animation.pause();
+    car.style.left = getComputedStyle(car).left;
+    animation.cancel();
+    clearTimeout(elPosition);
+    smoke.style.display = 'block';
+}
+
+async function status200(car: HTMLDivElement, time: number) {
+    winner = Number(car.id);
+    const add = winners.addWinner(winner, Math.round(time / 1000));
+    showModal(winner, Math.round(time / 1000));
+
+    if ((await add).status === 500) {
+        const get = (await winners.getWinner(winner)) as IWinner;
+        const winCount = get.wins + 1;
+        const bestTime = Math.round(time / 1000) > get.time ? get.time : Math.round(time / 1000);
+        winners.updateWinner(winner, winCount, bestTime);
+    }
+
+    showWinners();
+}
+
+function resetOrStop(
+    lW: Animation,
+    rW: Animation,
+    animation: Animation,
+    elPosition: ReturnType<typeof setTimeout>,
+    smoke: HTMLSpanElement,
+    resetClicked: boolean,
+    stopBtn: HTMLButtonElement
+) {
+    animation.cancel();
+    clearTimeout(elPosition);
+    lW.cancel();
+    rW.cancel();
+    smoke.style.display = 'none';
+    stopBtn.disabled = true;
+    resetClicked = true;
+}
+
+function getElements(
+    id: string,
+    stopBtns: NodeListOf<HTMLButtonElement>
+): [
+    HTMLDivElement,
+    HTMLSpanElement,
+    HTMLDivElement,
+    HTMLButtonElement,
+    HTMLSpanElement,
+    HTMLSpanElement,
+    HTMLSpanElement
+] {
+    const car = document.getElementById(id) as HTMLDivElement;
+    const icon = car.querySelector(`#car-${id}`) as HTMLSpanElement;
+    const parent = document.querySelector('.race-road') as HTMLDivElement;
+    const stopBtn = getButton(stopBtns, id);
+    const lWheel = icon.querySelector('.l-wheel') as HTMLSpanElement;
+    const rWheel = icon.querySelector('.r-wheel') as HTMLSpanElement;
+    const smoke = icon.querySelector('.car-smoke') as HTMLSpanElement;
+    return [car, icon, parent, stopBtn, lWheel, rWheel, smoke];
 }
